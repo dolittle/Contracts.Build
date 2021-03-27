29609897a293f2002429fd1d90945dbf4fb47ac2
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-03-27 14:58:16 +0100

    Add support for specifying output directory

--- Source/JavaScript/index.ts
+++ Source/JavaScript/index.ts
@@ -9,16 +9,18 @@ import { generateAction } from './generateAction';
 import { GenerationTarget } from './GenerationTarget';
 import { Generator } from './Generator';
 
-const generator = new Generator('./build');
+const generator = new Generator();
 
 const program = new Command('dolittle_proto_build');
 
+const output = new Option('-O <path>', 'Output path').default('./build');
 const includes = new Option('-I <path>', 'Include path (multiple allowed)');
 const rewrite = new Option('-R <rewrite>', 'Rewrite file paths (multiple allowed)');
 const skipEmptyFiles = new Option('--skip-empty-files', 'Remove files generated without any content');
 
 program
     .command('grpc-node <paths...>')
+    .addOption(output)
     .addOption(repeated(includes))
     .addOption(repeated(rewrite))
     .addOption(skipEmptyFiles)
@@ -27,6 +29,7 @@ program
 
 program
     .command('grpc-web <paths...>')
+    .addOption(output)
     .addOption(repeated(includes))
     .addOption(repeated(rewrite))
     .addOption(skipEmptyFiles)
